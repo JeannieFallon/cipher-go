da066e90111d1af21c5460f4b8faac73b789e286
Jeannie Fallon <jeanniefallon@gmail.com>
2019-08-13 06:54:00 -0400

    project structure fubar. fix after work

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-
-	"github.com/jeannie/cipher-go/ciphers"
+	"github.com/JeannieFallon/ciphers-go/internal/app/ciphers"
 )
 
-// TODO validation
-
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Println("Enter your plain text:")
-	plainTxt, _ := reader.ReadString('\n')
-
-	fmt.Println("Enter a key value for the Caesar cipher:")
-	keyStr, _ := reader.ReadString('\n')
-	keyVal, _ := strconv.Atoi(strings.TrimSpace(keyStr))
-
-	fmt.Println("Enter a key word for the Vigenere cipher:")
-	keyWord, _ := reader.ReadString('\n')
-
-	rot13Cipher := ciphers.GetRotThirteen(plainTxt)
-	caesarCipher := ciphers.GetCaesar(plainTxt, keyVal)
-	vigenereCipher := ciphers.GetVigenere(plainTxt, keyWord)
-
-	fmt.Println("\n* * * * *")
-	fmt.Printf("Rot-13 cipher text:\n%s\n", rot13Cipher)
-	fmt.Printf("Caesar cipher text:\n%s\n", caesarCipher)
-	fmt.Printf("Vigenere cipher text:\n%s\n", vigenereCipher)
+	ciphers.GetCiphers();
 }
